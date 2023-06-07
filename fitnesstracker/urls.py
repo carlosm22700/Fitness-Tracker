849bd5be6f2ceede3e4547a0a928c5f301e64bff
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-07 13:18:58 -0700

    accounts path url

--- fitnesstracker/urls.py
+++ fitnesstracker/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
