e66087ad38253d05a7bb7130cd3140a885761fc6
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-11 15:13:55 -0700

    Config Deployment

--- fitnesstracker/settings.py
+++ fitnesstracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Other settings above
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
